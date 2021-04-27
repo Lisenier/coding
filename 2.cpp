bf67dac4780484ac5c3600165a7db06a0ded7757
Lisenier <52318768+Lisenier@users.noreply.github.com>
2021-04-27 16:43:07 +0530

    pointers

---
+++ 2.cpp
@@ -0,0 +1,10 @@
+//Pointers in c++
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x= 10;
+    cout<<x<<endl; //prints the value of x
+    cout<<&x<<endl; //prints the address of x
+    cout<<*(&x)<<endl; //prints the value of x
+}
